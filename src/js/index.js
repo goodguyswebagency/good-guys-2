588bd49d646efa6aa5f59df418e704012caa467f
Milos-GoodGuy <milos@goodguys.nu>
2024-10-21 13:35:05 +0200

    edit noopener noreferrer nofollow links

--- src/js/index.js
+++ src/js/index.js
@@ -37,7 +37,7 @@ function setRelAttribute() {
   var elems = document.body.getElementsByTagName('a');
 	for (var i = 0; i < elems.length; i++) {
 		var elem = elems[i]
-		var re = /www.goodguys.nu/
+		var re = /www.goodguys.se/
 		var isInternal = re.test(elem.href)
 		if (!isInternal) {
 		elem.rel= 'noopener noreferrer nofollow'
