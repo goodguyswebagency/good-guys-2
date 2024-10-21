6831e3e229d96ed9b2f3a7ac77c65f67a512eaea
Milos-GoodGuy <milos@goodguys.nu>
2024-10-21 10:45:14 +0200

    og url dynamic

--- src/js/index.js
+++ src/js/index.js
@@ -55,3 +55,24 @@ $('img').each(function(){
 	$(this).removeAttr('srcset');
 });
 
+
+/////////////////////////////////////////
+// og:url change for other languages
+////////////////////////////////////////
+const urlPath = window.location.origin + window.location.pathname;
+
+// Check for the meta og:url tag
+const ogURL = document.querySelector('meta[property="og:url"]');
+
+// Add the meta og:url tag if not found
+if (!ogURL) {
+  const meta = document.createElement('meta');
+  meta.setAttribute('property', 'og:url');
+  meta.setAttribute('content', urlPath);
+  document.head.appendChild(meta);
+} else {
+  // If the og:url meta tag already exists, update its content
+  ogURL.setAttribute('content', urlPath);
+}
+
+
